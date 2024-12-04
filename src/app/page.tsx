eeee6b67257c640905a4b0af224465ccffdf621d
Shefat Ahmed <shefata69@gmail.com>
2024-12-04 12:03:39 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>This is HomePage</h1>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+};
+
+export default HomePage;
