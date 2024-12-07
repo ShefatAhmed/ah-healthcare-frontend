74b8c24933245b051a32021738099e28f28039aa
Shefat Ahmed <shefata69@gmail.com>
2024-12-07 16:02:07 +0600

    Retrieve and decode user info from local storage

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { getUserInfo } from "@/services/auth.services";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 
 const Navbar = () => {
+  const userInfo = getUserInfo();
+  console.log(userInfo);
   return (
     <Container>
       <Stack
@@ -18,15 +22,29 @@ const Navbar = () => {
           Health Care
         </Typography>
         <Stack direction="row" justifyContent="space-between" gap={4}>
-          <Typography component={Link} href="/consultation">Consultation</Typography>
-          <Typography component={Link} href="/health-plans">Health Plans</Typography>
-          <Typography component={Link} href="/medicine">Medicine</Typography>
-          <Typography component={Link} href="/diagnostics">Diagnostics</Typography>
-          <Typography component={Link} href="/ngos">NGOs</Typography>
+          <Typography component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography component={Link} href="/health-plans">
+            Health Plans
+          </Typography>
+          <Typography component={Link} href="/medicine">
+            Medicine
+          </Typography>
+          <Typography component={Link} href="/diagnostics">
+            Diagnostics
+          </Typography>
+          <Typography component={Link} href="/ngos">
+            NGOs
+          </Typography>
         </Stack>
-        <Button component={Link} href="/login">
-          Login
-        </Button>
+        {userInfo?.userId ? (
+          <Button color="error">Logout</Button>
+        ) : (
+          <Button component={Link} href="/login">
+            Login
+          </Button>
+        )}
       </Stack>
     </Container>
   );
