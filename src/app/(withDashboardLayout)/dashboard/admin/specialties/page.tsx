2848f30d8cfb0beab2feb334dc82b730c31dbdde
Shefat Ahmed <shefata69@gmail.com>
2024-12-10 12:37:23 +0600

    Creating GET API Endpoint for Specialties with Redux Caching

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -2,9 +2,11 @@
 import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 import SpecialtyModal from "./components/SpecialistModal";
+import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const {data, isLoading} = useGetAllSpecialtiesQuery({})
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
